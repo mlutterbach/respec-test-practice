144b0d9c148c87dd9df1ff1c7c25c93c62712530
Marcos Lutterbach <mlutterb@gmail.com>
2025-01-16 23:52:32 +0100

    Instructions

--- lib/generate_diagonal.rb
+++ lib/generate_diagonal.rb
@@ -1,3 +1,11 @@
+# Create a function that returns an array containing the first l numbers from the nth diagonal of Pascal's triangle.
+
+#cn = 0 should generate the first diagonal of the triangle (the ones).
+# The first number in each diagonal should be 1.
+# If l = 0, return an empty array.
+# Both n and l will be non-negative integers in all test cases.
+
+
 def fact(n)
   return 1 if n == 0
   (1..n).reduce(:*)
